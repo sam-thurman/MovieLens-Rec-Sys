90c3192321e9d358fb31ac70020333d0697d38a2
jarodc33 <jarodc333@gmail.com>
2020-04-15 11:34:01 -0700

    fixed compare all movies

--- src/metrics.py
+++ src/metrics.py
@@ -11,7 +11,7 @@ def compare_movie(movie_1, movie_2, encoded_genres = None):
         movie_1 = encoded_genres[encoded_genres['movieId'] == movie_1].drop(['movieId', 'title'], axis = 1)
     if isinstance(movie_2, int):    
         movie_2 = encoded_genres[encoded_genres['movieId'] == movie_2].drop(['movieId', 'title'], axis = 1)
-    return cosine_similarity(movie_1, movie_2)
+    return round(cosine_similarity(movie_1, movie_2)[0][0], 5)
 
 def compare_all_movies(movieId, encoded_genres):
     '''
@@ -24,6 +24,6 @@ def compare_all_movies(movieId, encoded_genres):
     movie_vector = movie.drop(['movieId', 'title'], axis = 1)
     lst = []
     for _, movie in encoded_genres.drop(index).iterrows():
-        lst.append(compare_movie(movie_vector, [movie.drop(['movieId', 'title'])]))
-    lst.sort(reverse = True)
-    return lst
+        lst.append((encoded_genres.loc[_]['movieId'], compare_movie(movie_vector, [movie.drop(['movieId', 'title'])])))
+    lst.sort(reverse = True, key = lambda x: x[1])
+    return pd.DataFrame(lst, columns = ['movieId', 'similarity'])
